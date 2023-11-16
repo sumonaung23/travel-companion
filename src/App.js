7ad74b99d6cee283e735f237dfad23671cf22d74
sumonaung23 <sumonaung575@gmail.com>
2023-11-16 23:42:38 +0630

    created starter files for travel Companion app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Header from './components/Header/Header'
+import List from './components/List/List'
+import Map from './components/Map/Map'
+
+const App = () => {
+    return (
+        <div>App</div>
+    )
+}
+
+export default App
