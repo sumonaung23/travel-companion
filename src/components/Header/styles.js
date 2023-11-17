12fe3745c0a419924649b7c943ef542830af6897
sumonaung23 <sumonaung575@gmail.com>
2023-11-17 23:54:20 +0630

    styled the header components inside js function with mui components

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,9 +1,33 @@
-import React from "react";
+import { makeStyles } from '@mui/styles';
+import { alpha } from '@mui/material/styles';
 
-const Header = () => {
-    return (
-        <h1>Header</h1>
-    )
-}
-
-export default Header;
+export default makeStyles((theme) => ({
+        title: {
+            display: 'none',
+            [theme.breakpoints.up('sm')]: {
+                display: 'block',
+            },
+        },
+        search: {
+            position: 'relative',
+            borderRadius: theme.shape.borderRadius,
+            backgroundColor: alpha(theme.palette.common.white, 0.15),
+            '&:hover': { backgroundColor: alpha(theme.palette.common.white, 0.25) },
+            marginRight: theme.spacing(2),
+            marginLeft: 0,
+            width: '100%',
+            [theme.breakpoints.up('sm')]: { marginLeft: theme.spacing(3), width: 'auto' },
+        },
+        searchIcon: {
+            gnItems: 'center', justifyContent: 'center',
+        },  
+        inputRoot: {
+            lor: 'inherit',
+        },
+        inputInput: {
+            padding: theme.spacing(1, 1, 1, 0), paddingLeft: `calc(1em + ${theme.spacing(4)}px)`, transition: theme.transitions.create('width'), width: '100%', [theme.breakpoints.up('md')]: { width: '20ch' },
+        },
+        toolbar: {
+            display: 'flex', justifyContent: 'space-between',
+        },
+    }));
