fc6452d9fea7f38ba067949d244dae7832afc9ac
sumonaung23 <sumonaung575@gmail.com>
2023-11-17 23:55:15 +0630

    modified index.js with latest version of rendering the root

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from "react";
-import { ReactDOM } from "react";
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/* ReactDOM.render(<App />, document.getElementById('root')); */
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render (
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+)
